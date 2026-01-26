b0e110193600773fcf768c7188dfa9e9146fdb4d
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-01-26 22:38:14 +0300

    Add weapon shot vibration

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -1574,6 +1574,9 @@ void CWeaponMagazined::OnShot()
 	// Camera	
 	AddShotEffector();
 
+	if (H_Parent() && H_Parent() == Level().CurrentControlEntity())
+		pInput->feedback(65535, 65535, 0.1f);
+
 	// Animation
 	PlayAnimShoot();
 
