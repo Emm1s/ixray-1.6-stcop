2a6cdacaf09606db86537b9001c7bef2bddd9232
mnelenpridumivat <ragrind@mail.ru>
2026-02-14 21:16:43 +0300

    Add check for invalid face in model surface

--- src/xrCore/LocatorAPI.cpp
+++ src/xrCore/LocatorAPI.cpp
@@ -1523,14 +1523,18 @@ T *CLocatorAPI::r_open_impl	(const char* path, const char* _fname)
 		file_from_cache(R, fname, sizeof(fname), *desc, source_name);
 	}
 	else
+	{
 		file_from_archive	(R,fname,*desc);
+	}
 
 #ifdef DEBUG
 	if (R && m_Flags.is(flBuildCopy|flReady))
+	{
 		copy_file_to_build	(R,source_name);
+	}
 #endif // DEBUG
 
-	return (R);
+	return R;
 }
 
 CStreamReader* CLocatorAPI::rs_open(const char* path, const char* _fname)
