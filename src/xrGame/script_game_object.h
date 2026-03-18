a0bec532ff644cb1027c2ce68686604cb9ff01d5
mnelenpridumivat <ragrind@mail.ru>
2026-03-18 22:07:05 +0300

    External storage for traders

--- src/xrGame/script_game_object.h
+++ src/xrGame/script_game_object.h
@@ -1034,6 +1034,9 @@ public:
 	bool IsActorLadder() const;
 	bool IsActorSafemode() const;
 	void SetActorSafemode(bool status);
+
+	bool UseExternalStorageForTrade();
+	
 	doors::door* m_door;
 
 	DECLARE_SCRIPT_REGISTER_FUNCTION
