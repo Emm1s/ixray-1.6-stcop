2ad10f10fe128f07ffc8a1d0135c6e43dfa653ca
ForserX <sv3nk@yandex.ru>
2026-04-19 02:06:50 +0300

    шлинукс

--- src/xrCore/_types.h
+++ src/xrCore/_types.h
@@ -23,13 +23,6 @@ using pcstr = const char*;
 
 struct xr_empty {};
 
-// windoze stuff
-#ifndef _WINDOWS_
-typedef	int				BOOL;
-#define TRUE			true
-#define FALSE			false
-#endif
-
 // Type limits
 #define type_max(T)		(std::numeric_limits<T>::max())
 #define type_min(T)		(-std::numeric_limits<T>::max())
@@ -42,8 +35,7 @@ typedef	int				BOOL;
 
 #define flt_max			type_max(float)
 #define flt_min			type_min(float)
-//#define FLT_MAX         3.402823466e+38F        /* max value */
-//#define FLT_MIN         1.175494351e-38F        /* min positive value */
+
 #define FLT_MAX			flt_max
 #define FLT_MIN			flt_min
 
