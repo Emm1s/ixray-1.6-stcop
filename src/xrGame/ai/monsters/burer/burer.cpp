7f82450b3d2cb19c04e5e901a3c6514a0c20c7b8
Aphile <taiko6661@gmail.com>
2026-05-14 23:54:21 +0500

    Добавить фикс для SoC.

--- src/xrGame/ai/monsters/burer/burer.cpp
+++ src/xrGame/ai/monsters/burer/burer.cpp
@@ -68,13 +68,13 @@ void CBurer::reload(const char* section)
 	}
 
 	// add specific sounds
-	sound().add				(pSettings->r_string(section,"sound_gravi_attack"),	DEFAULT_SAMPLE_COUNT,	
-							SOUND_TYPE_MONSTER_ATTACKING,	MonsterSound::eHighPriority + 2,	
-							u32(MonsterSound::eBaseChannel),	eMonsterSoundGraviAttack, "head");
+	sound().add(pSettings->r_string(section, "sound_gravi_attack"), DEFAULT_SAMPLE_COUNT,
+	            SOUND_TYPE_MONSTER_ATTACKING, MonsterSound::eHighPriority + 2,
+	            u32(MonsterSound::eBaseChannel), eMonsterSoundGraviAttack, get_head_bone_name());
 
-	sound().add				(pSettings->r_string(section,"sound_tele_attack"),	DEFAULT_SAMPLE_COUNT,	
-							SOUND_TYPE_MONSTER_ATTACKING,	MonsterSound::eHighPriority + 3,	
-							u32(MonsterSound::eBaseChannel),	eMonsterSoundTeleAttack, "head");
+	sound().add(pSettings->r_string(section, "sound_tele_attack"), DEFAULT_SAMPLE_COUNT,
+	            SOUND_TYPE_MONSTER_ATTACKING, MonsterSound::eHighPriority + 3,
+	            u32(MonsterSound::eBaseChannel), eMonsterSoundTeleAttack, get_head_bone_name());
 }
 
 void CBurer::ActivateShield()
