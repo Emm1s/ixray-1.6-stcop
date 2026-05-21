9943e7b4b580b782eaeebbf0ce09e0c578cdb003
mnelenpridumivat <ragrind@mail.ru>
2026-05-22 01:24:09 +0300

    Save data about external objects for xrLC -> "Option to save static object separately from level"

--- src/Editors/LevelEditor/Engine/communicate.h
+++ src/Editors/LevelEditor/Engine/communicate.h
@@ -157,6 +157,13 @@ struct b_mu_reference
     u32					reserved	[8];
 };
 
+struct b_external_object_reference
+{
+	string128 name;
+	Fmatrix transform;
+	u16 sector;
+};
+
 struct b_params
 {
 	// Normals & optimization
@@ -260,6 +267,7 @@ enum EBUILD_CHUNKS
     EB_MU_refs,
     EB_SmoothGroups,
 	EB_MaterialsShared,
+	EB_ExternalObjects,
 
 	EB_FORCE_DWORD = u32(-1)
 };
