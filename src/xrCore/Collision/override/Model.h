45d0549643ccec8bce631b6749562b016511d59e
mnelenpridumivat <ragrind@mail.ru>
2026-07-01 01:56:02 +0300

    Implementation of custom BVH collision structure

--- src/xrCore/Collision/override/Model.h
+++ src/xrCore/Collision/override/Model.h
@@ -4,6 +4,103 @@
 // Update : 20.04.2020 - Support for Hybrid Trees System 
 //////////////////////////////////////////////////////////
 #pragma once
+#include "Collision/xrCDB.h"
+
+namespace CDB
+{
+	class BVHNode;
+
+	union ElementID
+	{
+		BVHNode* p;
+		struct
+		{
+			size_t Index:62;
+			size_t IsInstance:1;
+			size_t IsNotPointer:1;
+		};
+	};
+	static_assert(sizeof(ElementID) == sizeof(size_t));
+
+	class BVHNode
+	{
+		Fbox AABB;
+		ElementID ElemPos = {};
+		ElementID ElemNeg = {};
+	public:
+		
+		const Fbox& GetAABB() const { return AABB; }
+		Fbox& GetAABB() { return AABB; }
+		
+		ElementID& GetPos() { return ElemPos; }
+		ElementID& GetNeg() { return ElemNeg; }
+		
+		bool HasPosNode() const { return !ElemPos.IsNotPointer; }
+		const BVHNode& GetPosNode() const
+		{
+			IVERIFY(HasPosNode());
+			return *ElemPos.p;
+		}
+		BVHNode& GetPosNode()
+		{
+			IVERIFY(HasPosNode());
+			return *ElemPos.p;
+		}
+		
+		bool HasNegNode() const { return !ElemNeg.IsNotPointer; }
+		const BVHNode& GetNegNode() const
+		{
+			IVERIFY(HasNegNode());
+			return *ElemNeg.p;
+		}
+		BVHNode& GetNegNode()
+		{
+			IVERIFY(HasNegNode());
+			return *ElemNeg.p;
+		}
+	};
+
+	class XRCORE_API BVHModel
+	{
+		xr_vector<BVHNode> Nodes;
+		Fbox AABB;
+		
+	public:		
+		void Store(IWriter* pWriter);
+		bool Restore(IReader* pReader);
+		
+		const Fbox& GetAABB() const { return AABB; }
+		Fbox& GetAABB() { return AABB; }
+		const xr_vector<BVHNode>& GetNodes() const { return Nodes; }
+		xr_vector<BVHNode>& GetNodes() { return Nodes; }
+	};
+
+	struct BuilderConfig
+	{
+		struct InstanceData
+		{
+			Fmatrix Transform;
+			size_t ModelIndex;
+		};
+		xr_vector<Fvector>* Vertices;
+		xr_vector<TRI>* Faces;
+		xr_vector<BVHModel*>* Models;
+		xr_vector<InstanceData>* Instances;
+		
+		Dvector GetPrimitiveMean(size_t Index) const;
+		void GetPrimitiveAABB(size_t Index, Dbox& Out) const;
+		double GetPrimitivePosOnAxis(size_t Index, Vector3Axes Axis) const;
+		bool IsPrimitiveIndex(size_t Index) const;
+		bool IsInstanceIndex(size_t Index) const;
+		
+	private:
+		void GetInstanceAABB(size_t Index, Dbox& Out) const;
+	};
+
+	BVHModel* BuildModel(const BuilderConfig& config);
+}
+
+
 #include <Opcode.h>
 
 class CDB_OptimizeTree;
