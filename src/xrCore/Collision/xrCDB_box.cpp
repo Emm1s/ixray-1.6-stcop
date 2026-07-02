98e8385bf3ce62f383e4f2e0e0874c740910138e
mnelenpridumivat <ragrind@mail.ru>
2026-07-02 14:34:10 +0300

    Revert "(WIP) Add support in queries -> "(WIP) Add Instanced collision"""
    This reverts commit 74f2e4620ebbde9eb35d83192d067e88ff2d74ce.

--- src/xrCore/Collision/xrCDB_box.cpp
+++ src/xrCore/Collision/xrCDB_box.cpp
@@ -7,30 +7,13 @@
 using namespace CDB;
 using namespace Opcode;
 
-constexpr size_t MAX_INSTANCE_DEPTH = 4;
-
-struct cform_stack final
-{
-	xr_array<const MODEL*, MAX_INSTANCE_DEPTH> m_def_array = {};
-	size_t CurrentIndex = 0;
-	
-	cform_stack(const MODEL& RootModel){ m_def_array[0] = &RootModel; }
-	
-	ICF const MODEL& GetCurrentTree()
-	{
-		VERIFY(CurrentIndex < m_def_array.size());
-		VERIFY(m_def_array[CurrentIndex]);
-		return *m_def_array[CurrentIndex];
-	}
-	
-	ICF void Push(const MODEL& NewTree){ CurrentIndex++; VERIFY(CurrentIndex < m_def_array.size()); m_def_array[CurrentIndex] = &NewTree; }
-	ICF void Pop(){ m_def_array[CurrentIndex] = nullptr; VERIFY(CurrentIndex > 0); CurrentIndex--;  }
-};
-
 struct cform_box_collider final
 {
-	xr_vector<RESULT>* dest = nullptr;
-	cform_stack* stack = nullptr;
+	COLLIDER* dest;
+	
+	xr_array<const MODEL*, 4> m_def_array = {};
+	size_t CurrentIndex = 0;
+	
 	Fbox box;
 	bool bClass3, bFirst;
 
@@ -91,6 +74,13 @@ struct cform_box_collider final
 			_prim(node.GetNeg());
 		}
 	}
+private:
+	
+	ICF const MODEL& GetCurrentTree()
+	{
+		VERIFY(m_def_array[CurrentIndex]);
+		return *m_def_array[CurrentIndex];
+	}
 };
 
 void COLLIDER::box_query(const MODEL *m_def, const Fbox& _box)
@@ -107,10 +97,9 @@ void COLLIDER::box_query(const MODEL *m_def, const Fbox& _box)
 	// Get nodes
 	auto& Nodes = m_def->tree->GetNodes();
 
-	cform_stack S = *m_def;
 	cform_box_collider BC{};
 	BC.dest = this;
-	BC.stack = &S;
+	BC.m_def_array[0] = m_def;
 	BC.box = _box;
 	BC.bClass3 = box_mode & OPT_FULL_TEST;
 	BC.bFirst = box_mode & OPT_ONLYFIRST;
@@ -119,69 +108,15 @@ void COLLIDER::box_query(const MODEL *m_def, const Fbox& _box)
 
 struct cform_obb_collider final
 {
-	xr_vector<RESULT>* dest = nullptr;
-	cform_stack* stack = nullptr;
-	
+	COLLIDER* dest;
+	const MODEL *m_def;
 	Fobb obb;
 
 	bool bClass3 = false;
 	bool bFirst = false;
 
-	ICF void _prim(ElementID prim)
+	ICF void _prim(size_t prim)
 	{
-		VERIFY(prim.IsNotPointer);
-		if (prim.IsInstance)
-		{
-			auto& CurModel = stack->GetCurrentTree();
-			auto& Instances = CurModel.get_instances();
-			VERIFY(prim.Index < Instances.size());
-			auto& InstanceInvTransform = Instances[prim.Index].InvTransform;
-			auto& InstanceModel = CurModel.get_models()[Instances[prim.Index].ModelIndex];
-			stack->Push(InstanceModel);
-			
-			Fobb localOBB;
-			localOBB.transform(obb, InstanceInvTransform);
-			
-			if (false /*no rot and scale*/)
-			{
-				
-			}
-			else if (false /*uniform transform*/)
-			{
-				
-			}
-			else
-			{
-				xr_vector<RESULT> RawResults = {};
-				cform_box_collider InstanceCollider{
-					&RawResults,
-					stack,
-					{},
-					bClass3,
-					bFirst,
-				};
-				
-				Fvector aX = localOBB.m_rotate.i * localOBB.m_halfsize.x;
-				Fvector aY = localOBB.m_rotate.j * localOBB.m_halfsize.y;
-				Fvector aZ = localOBB.m_rotate.k * localOBB.m_halfsize.z;
-				
-				InstanceCollider.box.modify(localOBB.m_translate + aX + aY + aZ);
-				InstanceCollider.box.modify(localOBB.m_translate + aX + aY - aZ);
-				InstanceCollider.box.modify(localOBB.m_translate + aX - aY + aZ);
-				InstanceCollider.box.modify(localOBB.m_translate + aX - aY - aZ);
-				InstanceCollider.box.modify(localOBB.m_translate - aX + aY + aZ);
-				InstanceCollider.box.modify(localOBB.m_translate - aX + aY - aZ);
-				InstanceCollider.box.modify(localOBB.m_translate - aX - aY + aZ);
-				InstanceCollider.box.modify(localOBB.m_translate - aX - aY - aZ);
-				
-				_stab(InstanceModel.tree->GetNodes()[0]);
-				
-				
-				
-			}
-			
-			return;
-		}
 		auto& Tri = tris[prim];
 		auto& TriVerts = Tri.verts;
 		Fvector tri_verts[3] = { verts[TriVerts[0]], verts[TriVerts[1]], verts[TriVerts[2]] };
@@ -197,42 +132,21 @@ struct cform_obb_collider final
 		R.dummy = Tri.dummy;
 	}
 
-	void _stab(const BVHNode& node)
+	void _stab(const AABBNoLeafNode* node)
 	{
-		VERIFY(dest);
-		VERIFY(stack);
 		// Actual OBB-AABB test
-		
-		if (!obb.intersectAABB(node.GetAABB()))
-		{
-			return;
-		}
+		if (!obb.intersectAABB((Fvector&)node->mAABB.mCenter, (Fvector&)node->mAABB.mExtents)) return;
 
 		// 1st child
-		if (node.HasPosNode())
-		{
-			_stab(node.GetPosNode());
-		}
-		else
-		{
-			_prim(node.GetPos());
-		}
+		if (node->HasPosLeaf())	_prim(node->GetPosPrimitive());
+		else					_stab(node->GetPos());
 
 		// Early exit for "only first"
-		if (bFirst && dest->r_count())
-		{
-			return;
-		}
+		if (bFirst && dest->r_count()) return;
 
 		// 2nd child
-		if (node.HasNegNode())
-		{
-			_stab(node.GetNegNode());
-		}
-		else
-		{
-			_prim(node.GetNeg());
-		}
+		if (node->HasNegLeaf())	_prim(node->GetNegPrimitive());
+		else					_stab(node->GetNeg());
 	}
 };
 
@@ -251,11 +165,10 @@ void COLLIDER::obb_query(const MODEL* m_def, const Fobb& obb)
 	r_clear();
 	r_vec().reserve(16);
 
-	cform_stack S = *m_def;
 	cform_obb_collider OC
 	{
-		&rd,
-		&S,
+		this,
+		m_def,
 		obb,
 		!!(obb_mode & OPT_FULL_TEST),
 		!!(obb_mode & OPT_ONLYFIRST)
